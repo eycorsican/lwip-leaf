d20eaab2f64dcb6573a6f92608ee88b89aee578a
eric <eric.y.corsican@gmail.com>
2020-10-17 08:26:02 +0800

    update unix port header

--- custom/arch/cc_others.h
+++ custom/arch/cc_others.h
@@ -32,68 +32,55 @@
 #ifndef LWIP_ARCH_CC_H
 #define LWIP_ARCH_CC_H
 
-/* Include some files for defining library routines */
-#include <sys/time.h>
-
-#define LWIP_TIMEVAL_PRIVATE 0
-
-/* Define platform endianness */
-#ifndef BYTE_ORDER
-#define BYTE_ORDER LITTLE_ENDIAN
-#endif /* BYTE_ORDER */
-
-/* Define generic types used in lwIP */
-/*
-typedef unsigned   char    u8_t;
-typedef signed     char    s8_t;
-typedef unsigned   short   u16_t;
-typedef signed     short   s16_t;
-typedef unsigned   int     u32_t;
-typedef signed     int     s32_t;
-
-typedef unsigned long mem_ptr_t;
-*/
-
-/* Define (sn)printf formatters for these lwIP types */
-#define X8_F  "02x"
-#define U16_F "hu"
-#define S16_F "hd"
-#define X16_F "hx"
-#define U32_F "u"
-#define S32_F "d"
-#define X32_F "x"
-
-/* If only we could use C99 and get %zu */
-#if defined(__x86_64__)
-#define SZT_F "lu"
-#else
-#define SZT_F "u"
+/* see https://sourceforge.net/p/predef/wiki/OperatingSystems/ */
+#if defined __ANDROID__
+#define LWIP_UNIX_ANDROID
+#elif defined __linux__
+#define LWIP_UNIX_LINUX
+#elif defined __APPLE__
+#define LWIP_UNIX_MACH
+#elif defined __OpenBSD__
+#define LWIP_UNIX_OPENBSD
+#elif defined __CYGWIN__
+#define LWIP_UNIX_CYGWIN
+#elif defined __GNU__
+#define LWIP_UNIX_HURD
 #endif
 
-/* Compiler hints for packing structures */
-#define PACK_STRUCT_FIELD(x) x
-#define PACK_STRUCT_STRUCT __attribute__((packed))
-#define PACK_STRUCT_BEGIN
-#define PACK_STRUCT_END
+#define LWIP_TIMEVAL_PRIVATE 0
+#include <sys/time.h>
 
-/* prototypes for printf() and abort() */
-#include <stdio.h>
-#include <stdlib.h>
-/* Plaform specific diagnostic output */
-#define LWIP_PLATFORM_DIAG(x)	do {printf x;} while(0)
+#define LWIP_ERRNO_INCLUDE <errno.h>
 
-#ifdef LWIP_UNIX_EMPTY_ASSERT
-#define LWIP_PLATFORM_ASSERT(x)
-#else
-#define LWIP_PLATFORM_ASSERT(x) do {printf("Assertion \"%s\" failed at line %d in %s\n", \
-x, __LINE__, __FILE__); fflush(NULL); abort();} while(0)
+#if defined(LWIP_UNIX_LINUX) || defined(LWIP_UNIX_HURD)
+#define LWIP_ERRNO_STDINCLUDE	1
 #endif
 
 #define LWIP_RAND() ((u32_t)rand())
+
+/* different handling for unit test, normally not needed */
+#ifdef LWIP_NOASSERT_ON_ERROR
+#define LWIP_ERROR(message, expression, handler) do { if (!(expression)) { \
+  handler;}} while(0)
+#endif
+
+#if defined(LWIP_UNIX_ANDROID) && defined(FD_SET)
+typedef __kernel_fd_set fd_set;
+#endif
+
+#if defined(LWIP_UNIX_MACH)
+/* sys/types.h and signal.h bring in Darwin byte order macros. pull the
+   header here and disable LwIP's version so that apps still can get
+   the macros via LwIP headers and use system headers */
+#include <sys/types.h>
+#define LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS
+#endif
 
 struct sio_status_s;
 typedef struct sio_status_s sio_status_t;
 #define sio_fd_t sio_status_t*
 #define __sio_fd_t_defined
+
+typedef unsigned int sys_prot_t;
 
 #endif /* LWIP_ARCH_CC_H */
